9ec94790a7b7207ee9dea32e8bd7b1b042ba003a
Daniel (dmilith) Dettlaff <daniel@centra.com>
2020-06-22 14:48:41 +0200

    new: Created basic krecik-cli using krecik library.

---
+++ src/main.rs
@@ -0,0 +1,118 @@
+//! Crate docs
+
+#![forbid(unsafe_code)]
+#![deny(
+    missing_docs,
+    unstable_features,
+    unsafe_code,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications
+)]
+// For development:
+#![allow(dead_code, unused_imports, unused_variables)]
+
+#[macro_use]
+extern crate log;
+
+use chrono::*;
+use colored::Colorize;
+use krecik::api::*;
+use krecik::products::story::Story;
+use log::*;
+use std::io::{Error, ErrorKind};
+use std::{env, env::var, path::Path};
+
+
+fn setup_logger(level: LevelFilter) -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(level)
+        .chain(std::io::stdout())
+        .apply()?;
+    Ok(())
+}
+
+
+fn main() -> Result<(), Error> {
+    let logger_level = match var("DEBUG") {
+        Ok(value) => LevelFilter::Debug,
+        Err(_) => LevelFilter::Info,
+    };
+    setup_logger(logger_level).unwrap_or_default();
+
+    if env::args().len() == 0 {
+        Err(Error::new(
+            ErrorKind::InvalidData,
+            "You have to specify path(s) to json check file!",
+        ))
+    } else {
+        for check_file in env::args().skip(1).collect::<Vec<String>>() {
+            if Path::new(&check_file).exists() {
+                info!("Loading check from: {}", &check_file);
+                let history = execute_checks_from_file(&check_file);
+                debug!("History: {:?}", history);
+                for story in history.stories() {
+                    match story {
+                        Story {
+                            success: Some(success_msg),
+                            error: None,
+                            ..
+                        } => info!("Check SUCCESS: {}", success_msg.to_string().green()),
+
+                        Story {
+                            success: None,
+                            error: Some(error_msg),
+                            ..
+                        } => {
+                            error!(
+                                "Web-page Story of: FAILURE: {}",
+                                error_msg.to_string().red()
+                            )
+                        }
+
+                        Story {
+                            success: None,
+                            error: None,
+                            ..
+                        } => {
+                            warn!(
+                                "Web-page Story of: WARNING: {}",
+                                "Ambiguous Story that lacks both success and error?!".yellow()
+                            )
+                        }
+
+                        Story {
+                            success: Some(_),
+                            error: Some(_),
+                            ..
+                        } => {
+                            warn!(
+                                "Web-page Story of: WARNING: {}",
+                                "Ambiguous Story with success and failure at the same time?!"
+                                    .yellow()
+                            )
+                        }
+                    }
+                }
+            } else {
+                return Err(Error::new(
+                    ErrorKind::NotFound,
+                    format!("Check file not found: {}", check_file),
+                ));
+            }
+        }
+        Ok(())
+    }
+}
