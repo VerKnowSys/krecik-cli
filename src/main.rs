befc69314bde8784b55a3e0fd878dc84977e7928
Daniel (dmilith) Dettlaff <daniel@centra.com>
2020-07-11 16:29:03 +0200

    new: Implemented Ctrl-C handler to remove lock in case of interruption. Turned main() to return Result.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ extern crate log;
 use chrono::*;
 use colored::Colorize;
 use krecik::api::*;
+use lockfile::Lockfile;
 use log::*;
+use std::fs;
 use std::io::{Error, ErrorKind};
 use std::{env, env::var, path::Path};
 
@@ -44,24 +46,44 @@ fn setup_logger(level: LevelFilter) -> Result<(), fern::InitError> {
 }
 
 
-fn main() {
+fn main() -> Result<(), Error> {
     let logger_level = match var("DEBUG") {
         Ok(value) => LevelFilter::Debug,
         Err(_) => LevelFilter::Info,
     };
     setup_logger(logger_level).unwrap_or_default();
 
-    if env::args().len() == 1 {
-        error!("You have to specify path(s) to json check file!")
-    } else {
-        for check_file in env::args().skip(1).collect::<Vec<String>>() {
-            if Path::new(&check_file).exists() {
-                info!("Loading check from: {}", &check_file.green());
-                let history = execute_checks_from_file(&check_file);
-                debug!("History: {:?}", &history);
+    const LOCK_PATH: &str = "/tmp/krecik-remote-cli.lock";
+    ctrlc::set_handler(|| {
+        println!("Interrupted! Removing lock file: {}", LOCK_PATH);
+        fs::remove_file(LOCK_PATH).unwrap_or_default();
+        std::process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    Lockfile::create(LOCK_PATH)
+        .and_then(|lockfile| {
+            if env::args().len() == 1 {
+                error!("You have to specify path(s) to json check file!")
             } else {
-                error!("Check file not found: {}", &check_file.red())
+                for check_file in env::args().skip(1).collect::<Vec<String>>() {
+                    if Path::new(&check_file).exists() {
+                        info!("Loading check from: {}", &check_file.green());
+                        let history = execute_checks_from_file(&check_file);
+                        debug!("History: {:?}", &history);
+                    } else {
+                        error!("Check file not found: {}", &check_file.red())
+                    }
+                }
             }
-        }
-    }
+            lockfile.release()?; // or just let the lockfile be dropped
+            Ok(())
+        })
+        .map_err(|err| {
+            error!(
+                "Lock file acquired. Other instance is already running? Error: {}",
+                err
+            );
+            err
+        })
 }
